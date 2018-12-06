537b1f91908e1dfbe82a0bce88cd364734ec8c87
Jon Shaw <Jonshaw@quickenloans.com>
2018-12-06 16:55:51 -0500

    added user and item, can edit and delete users now

--- GCCoffeeShop/Controllers/DatabaseController.cs
+++ GCCoffeeShop/Controllers/DatabaseController.cs
@@ -18,6 +18,13 @@ namespace GCCoffeeShop.Controllers
             return View();
             
         }
+        public ActionResult ItemList()
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            ViewBag.ItemList = ORM.Items.ToList();
+            return View();
+
+        }
         public ActionResult AddUser()
         {
             return View();
@@ -35,14 +42,74 @@ namespace GCCoffeeShop.Controllers
                 ORM.SaveChanges();
             }
 
-            return RedirectToAction("Summary");
+
+            return RedirectToAction("Summary", newUser);
         }
         public ActionResult Summary(User user)
         {
-
-
             return View(user);
         }
+        public ActionResult EditUser(int UserID)
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            User found = ORM.Users.Find(UserID);
+
+            return View(found);
+        }
+        public ActionResult SaveUserChanges(User updatedUser)
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            User oldUser = ORM.Users.Find(updatedUser.UserID);
+            oldUser.FirstName = updatedUser.FirstName;
+            oldUser.LastName = updatedUser.LastName;
+            oldUser.Birthday = updatedUser.Birthday;
+            oldUser.Email = updatedUser.Email;
+            oldUser.Address = updatedUser.Address;
+            oldUser.PhoneNumber = updatedUser.PhoneNumber;
+            oldUser.Address = updatedUser.Address;
+            oldUser.City = updatedUser.City;
+            oldUser.PostalCode = updatedUser.PostalCode;
+            oldUser.State = updatedUser.State;
+
+            ORM.Entry(oldUser).State = System.Data.Entity.EntityState.Modified;
+            ORM.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteUser(int UserID)
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            User found = ORM.Users.Find(UserID);
+
+            ORM.Users.Remove(found);
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+        public ActionResult AddItem()
+        {
+            return View();
+        }
+        public ActionResult SaveItem(Item newItem)
+        {
+
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+
+
+            if (newItem != null)
+            {
+
+                ORM.Items.Add(newItem);
+                ORM.SaveChanges();
+            }
+
+            return RedirectToAction("ItemList");
+        }
+
 
     }
 }
